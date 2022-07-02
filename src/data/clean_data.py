6d8170c29832e2179b50c63f2517a0fd5c872447
CataMantilla <cmantil@unal.edu.co>
2022-07-02 08:51:18 -0500

    Respuesta

--- src/data/clean_data.py
+++ src/data/clean_data.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import os
+import glob
+
 def clean_data():
     """Realice la limpieza y transformación de los archivos CSV.
 
@@ -12,10 +16,35 @@ def clean_data():
 
 
     """
-    raise NotImplementedError("Implementar esta función")
+    cleansed_df = []
 
+    for f in range(1995, 2022):
+        df_read = pd.read_csv(
+            'data_lake/raw/{}.csv'.format(f),
+        )
+        cleansed_df.append(df_read)
+    
+    frame = pd.concat(cleansed_df, axis=0, ignore_index=True)
+    old_columns = frame.columns
+    new_columns = ['fecha'] + ['{0:0=2d}'.format(int(i)) for i in old_columns[1:]]
+    frame.columns = new_columns
+
+    frame['fecha'] = pd.to_datetime(frame['fecha'], format = '%Y-%m-%d')
+    unpivoted_table = frame.melt(
+        id_vars = ['fecha'],
+        value_vars = new_columns[1:],
+        var_name = 'hora',
+        value_name = 'precio',
+    )
+    unpivoted_table['precio'] = unpivoted_table['precio'].fillna(
+        unpivoted_table.groupby('fecha')['precio'].transform('mean')
+    )
+    unpivoted_table.to_csv(
+        'data_lake/cleansed/precios-horarios.csv', encoding='utf-8', index=False
+    )
 
 if __name__ == "__main__":
     import doctest
-
+    
+    clean_data()
     doctest.testmod()
