7e042bb4a4db16a9089e17cfe6e19444e6a4ba78
CataMantilla <cmantil@unal.edu.co>
2022-07-04 15:12:56 -0500

    respuesta

--- src/features/make_features.py
+++ src/features/make_features.py
@@ -1,15 +1,18 @@
+"""
+Crear archivo para el pronóstico.
+La función make_features
+Crea el archivo data_lake/business/features/precios-diarios.csv.Este
+archivo contiene la información para pronosticar los precios diarios de la
+electricidad con base en los precios de los días pasados. Las columnas
+correspoden a las variables explicativas del modelo, y debe incluir,
+adicionalmente, la fecha del precio que se desea pronosticar y el precio
+que se desea pronosticar (variable dependiente).
+"""
+import shutil
 def make_features():
-    """Crear archivo para el pronóstico.
-
-    Crear el archivo data_lake/business/features/precios-diarios.csv.Este
-    archivo contiene la información para pronosticar los precios diarios de la
-    electricidad con base en los precios de los días pasados. Las columnas
-    correspoden a las variables explicativas del modelo, y debe incluir,
-    adicionalmente, la fecha del precio que se desea pronosticar y el precio
-    que se desea pronosticar (variable dependiente).
-
-    """
-    import shutil
+    """Función make_features"""
+    
+    
 
     shutil.copy('data_lake/business/precios-diarios.csv',
                 'data_lake/business/features/precios_diarios.csv')
