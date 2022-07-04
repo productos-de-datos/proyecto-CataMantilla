acd0d8564dd1ef7c91b05bce1fb1c44b04099d8f
CataMantilla <cmantil@unal.edu.co>
2022-07-04 15:14:36 -0500

    respuesta

--- src/models/train_daily_model.py
+++ src/models/train_daily_model.py
@@ -6,16 +6,19 @@ Entrena el modelo de pronóstico de precios diarios usando
 salvados en la siguiente ruta:  models/precios-diarios.pkl
 los datos para este entrenamiento se encuentran en la ruta:
 data_lake/business/features/precios_diarios.csv
+Funciones usadas
 """
 def load_data():
+    """funcion load_data carga los datos"""
+    
     import pandas as pd
 
     in_path = 'data_lake/business/features/precios_diarios.csv'
     data = pd.read_csv(in_path, sep = ',')
-    
     return data
 
 def transform_data(data):
+    """función transform_data le da formato a los datos"""
     import pandas as pd
 
     df = data.copy()
@@ -23,36 +26,38 @@ def transform_data(data):
     df['year'], df['month'], df['day'] = df['fecha'].dt.year, df['fecha'].dt.month, df['fecha'].dt.day
     
     y = df['precio']
-    X = df.copy()
-    X.pop('precio')
-    X.pop('fecha')
-    return X, y
+    x = df.copy()
+    x.pop('precio')
+    x.pop('fecha')
+    return x, y
 
-def make_train_test_set(X, y):
+def make_train_test_set(x, y):
+    """Función make_train_test_set hace partición de datos"""
     from sklearn.model_selection import train_test_split
 
-    (X_train, X_test, y_train, y_test) = train_test_split(
-        X,
+    (x_train, x_test, y_train, y_test) = train_test_split(
+        x,
         y,
         test_size = 0.30,
         random_state = 12345,
     )
-    return X_train, X_test, y_train, y_test 
+    return x_train, x_test, y_train, y_test 
 
-def train_model(X_train, X_test):
+def train_model(x_train, x_test):
+    """train_model escala  los datos u crea el modelo"""
     from sklearn.preprocessing import StandardScaler
     from sklearn.ensemble import RandomForestRegressor
 
-    """Normalitation StandardScaler"""
+    
     scaler = StandardScaler()
-    X_train = scaler.fit_transform(X_train)
-    X_test = scaler.fit_transform(X_test)
+    x_train = scaler.fit_transform(x_train)
+    x_test = scaler.fit_transform(x_test)
 
-    """Crear modelo"""
     model = RandomForestRegressor(n_jobs=-1)
     return model
 
 def save_model(model):
+    """save_model guarda el modelo en el formato pickle"""
 
     import pickle
 
@@ -60,11 +65,12 @@ def save_model(model):
         pickle.dump(model, file,  pickle.HIGHEST_PROTOCOL)
 
 def train_daily_model():
+    """train_daily_model"""
     try:
         data = load_data()
-        X, y = transform_data(data)
-        X_train, X_test, y_train, y_test = make_train_test_set(X, y)
-        model = train_model(X_train, X_test)
+        x, y = transform_data(data)
+        x_train, x_test, y_train, y_test = make_train_test_set(x, y)
+        model = train_model(x_train, x_test)
         save_model(model)
     except:
         raise NotImplementedError("Implementar esta función")
